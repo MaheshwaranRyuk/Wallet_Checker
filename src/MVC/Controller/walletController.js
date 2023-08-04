4c1a1a7e92c2d2399b7566b03555b3d2df136d84
unknown <maheshwaran@ryuklabs.io>
2023-08-04 14:25:07 +0530

    controller optimization

--- src/MVC/Controller/walletController.js
+++ src/MVC/Controller/walletController.js
@@ -2,12 +2,8 @@ var QuestModel = require("../Model/walletModel");
 
 
 const saveQuest = async (req, res, next) => {
-//   if ((await Auth.authorizer(req, res)) == true) {
     var data = await QuestModel.saveQuest(req);
     res.status(data.statusCode).send(data);
-//   } else {
-//     res.status(400).send({ msg: "invalid sessions" });
-//   }
 };
 
 
